173025cb39255a0a7d1638a795fa2e6b497ae982
Jave <pangga.jave@gmail.com>
2021-04-11 12:12:26 +0800

    nav and side bar

--- files/layout/nav.view.php
+++ files/layout/nav.view.php
@@ -1,34 +1,40 @@
 
+
+
   <?php 
-  $nav = '<nav class="main-header navbar navbar-expand navbar-white navbar-light"> <ul class="navbar-nav ml-auto">';
-  if(isset($_SESSION['identity']) != null)
+  $nav = '  <nav class="main-header navbar navbar-expand navbar-white navbar-light"> 
+  <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+     </ul>
+  <ul class="navbar-nav ml-auto">
+
+  ';
+
+  if(!isset($_SESSION['identity']) != null)
   {
-    $nav .= '<li class="nav-item d-none d-sm-inline-block">'.
-            '<a href="" class="nav-link "><i class="fa fa-cog" aria-hidden="true"></i> Settings </a>'.
+    $nav .= '<a href="" class="nav-link "><i class="fa fa-user" aria-hidden="true"></i> Profile </a>'.
           '</li>'.
-          '<li class="nav-item d-none d-sm-inline-block">'.
+          '<li class="nav-item">'.
             '<a href="" class="nav-link ">|</a>'.
           '</li>'.
-          '<li class="nav-item d-none d-sm-inline-block">'.
-            '<a href="" class="nav-link "><i class="fa fa-user" aria-hidden="true"></i> Profile </a>'.
-          '</li>'.
-          '<li class="nav-item d-none d-sm-inline-block">'.
-            '<a href="" class="nav-link ">|</a>'.
-          '</li>'.
-          '<li class="nav-item d-none d-sm-inline-block">'.
+          '<li class="nav-item ">'.
             '<a href="" class="nav-link "><i class="fa fa-user" aria-hidden="true"></i> Logout </a>'.
           '</li>';
   }else{
-    $nav .= '<li class="nav-item d-none d-sm-inline-block">'.
+    $nav .= ' <li class="nav-item">'.
               '<a href="" class="nav-link "><i class="fa fa-user" aria-hidden="true"></i> Login </a>'.
             '</li>'.
-            '<li class="nav-item d-none d-sm-inline-block">'.
+            '<li class="nav-item">'.
               '<a href="" class="nav-link ">|</a>'.
             '</li>'.
-            '<li class="nav-item d-none d-sm-inline-block">'.
+            '<li class="nav-item">'.
               '<a href="" class="nav-link "><i class="fa fa-user" aria-hidden="true"></i> Register </a>'.
             '</li>';
   }
-  $nav .= '</ul></nav>';
+  $nav .= '</ul>
+  </nav>';
 
   echo $nav;
+
